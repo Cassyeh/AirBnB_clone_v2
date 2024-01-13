cad1edccfac1cea06202d111ff6f68c9432f29af
Cassyeh <cassandraijezie@gmail.com>
2024-01-13 10:56:58 -0800

    Full deployment of archive task

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
 """
-A script (based on the file 1-pack_web_static.py) that distributes
-an archive to your web servers
+A script that creates and distributes an archive to web servers
 """
 
 
 from fabric.api import *
 from datetime import datetime
 from os.path import exists
+# do_pack = __import__('1-pack_web_static').do_pack
+# do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 
 env.hosts = ['46.346.275.234', '65.276.72.310']
 # <IP web-01>, <IP web-02>
 # ^ All remote commands must be executed on your both web servers
 # (using env.hosts = ['<IP web-01>', 'IP web-02'] variable in your script)
+
+
+def do_pack():
+    """
+    Returns the .tgz archive path if archive has been correctly generated
+    else, return None
+    """
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+    if result.succeeded:
+        return filename
+    else:
+        return None
 
 
 def do_deploy(archive_path):
@@ -38,3 +54,14 @@ def do_deploy(archive_path):
         return True
     except Exception:
         return False
+
+
+def deploy():
+    """
+    Creates and distributes an archive to web servers
+    """
+    new_archive_path = do_pack()
+    if exists(new_archive_path) is False:
+        return False
+    result = do_deploy(new_archive_path)
+    return result
